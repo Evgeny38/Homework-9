c2a4fea2f08e5c46857a21ba3817bcf915ec52e4
E-RAY <reylan@yandex.ru>
2022-09-13 00:20:41 +0800

    первый коммит ДЗ объекты классы

--- src/Author.java
+++ src/Author.java
@@ -1,17 +1,13 @@
 public class Author {
     private final String name;
     private final String surname;
-//    private String Author;
+
 
 
     public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
-//    public String getAuthor(String name, String surname){
-//        return Author;
-                
-//    }
 
     public String getName() {
         return this.name;
